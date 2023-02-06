a2694b72045113dfb2866e654c33117f23609dc9
markmahn <55084874+markmahn@users.noreply.github.com>
2023-02-06 23:06:45 +0100

    initial + exit command

--- app.py
+++ app.py
@@ -2,25 +2,31 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Adjusting noise ")
-    recognizer.adjust_for_ambient_noise(source, duration=1)
-    print("Recording for 4 seconds")
-    recorded_audio = recognizer.listen(source, timeout=4)
-    print("Done recording")
-
-try:
-    print("Recognizing the text")
-    text = recognizer.recognize_google(
-            recorded_audio, 
-            language="en-US"
-        )
-
-    print("Decoded Text : {}".format(text))
-
-except Exception as ex:
-
-    print(ex)
+while (True):
+    with sr.Microphone() as source:
+        print("Adjusting noise ")
+        recognizer.adjust_for_ambient_noise(source, duration=0.5)
+        print("Recording ...")
+        recorded_audio = None
+        try:
+            recorded_audio = recognizer.listen(source, timeout=10)
+            print("Done recording")
+        except Exception as ex:
+            print("Sound of silence ...")
 
 
-sr.Microphone.list_microphone_names()
+    try:
+        print("Recognizing the text")
+        text = recognizer.recognize_google(
+                recorded_audio,
+                language="nl-NL"
+            )
+
+        print("You said: {}".format(text))
+        if("{}".format(text) == 'exit'):
+            print('exiting ...')
+            break
+
+    except Exception as ex:
+        print("")
+
